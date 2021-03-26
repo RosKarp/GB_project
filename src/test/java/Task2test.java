01edf2710cbafdd9a5b8c542c580e6a51785ace1
RosKarp <rostkar@gmail.com>
2021-03-26 14:45:08 +0300

    ДЗ Карпенко Ростислава к 7 уроку. Тестируются классы из 6 урока.

--- src/test/java/Task2test.java
+++ src/test/java/Task2test.java
@@ -1,25 +1,36 @@
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
 import ru.GeekBrains.Rostislav.Java3Group3.Lesson6.Task2;
 import java.util.Arrays;
 
 public class Task2test {
 
+    @BeforeSuite
+    public void BeforeSuite() {
+        System.out.println("Start Task2test.");
+    }
+
+    @MarkingAnnotation (value = 1)
     @Test
     public void test1Task2() {
         Assertions.assertArrayEquals(new int[]{5, 7}, Task2.afterLast4(new int[]{0, 8, 3, 4, 5, 7}));
     }
 
+    @MarkingAnnotation (value = 2)
     @Test
     public void test2Task2() {
         Assertions.assertArrayEquals(new int[]{1, 7}, Task2.afterLast4(new int[]{1, 2, 4, 4, 2, 3, 4, 1, 7}));
     }
 
+    @MarkingAnnotation (value = 3)
     @Test
     public void test3Task2() {
         Assertions.assertArrayEquals(new int[]{2, 3, 9, 1, 7}, Task2.afterLast4(new int[]{4, 2, 4, 4, 2, 3, 9, 1, 7}));
     }
 
+    @MarkingAnnotation (value = 4)
     @Test
     public void testExceptionTask2() { // как-то не очень, тестируется не метод целиком, а только его
         // модифицированная реализация
@@ -37,6 +48,7 @@ public class Task2test {
         });
     }
 
+    @MarkingAnnotation (value = 6)
     @Test
     public void testEmptyArrayTask2() { // как-то не очень, тестируется не метод целиком, а только его
         // модифицированная реализация
@@ -52,5 +64,10 @@ public class Task2test {
             }
             throw new RuntimeException();
         });
+    }
+
+    @AfterSuite
+    public void AfterSuite() {
+        System.out.println("Finish Task2test.");
     }
 }
